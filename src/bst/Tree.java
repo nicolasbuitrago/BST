23d3424904df5c8b90cdc61a34d3375125c8a793
nicolasbuitrago <nicolasbuitrago@uninorte.edu.co>
2017-08-28 12:07:35 -0500

    Borrados los metodos innecesarios y a medio terminar antes creados

--- src/bst/Tree.java
+++ src/bst/Tree.java
@@ -82,34 +82,6 @@ public class Tree
         }
     }
     
-    //El mio
-    public void removeN(Node root, int key){
-        Queue<Node> queue = new LinkedList();
-        queue.add(root);
-        while (!queue.isEmpty()) {            
-            Node temp = queue.poll();
-            
-            if (temp.left!=null) {
-//                if(temp.left.right)
-            }
-            if (temp.right!=null) {
-                queue.add(temp.right);
-            }
-        }
-    }
-    
-    public void buscar(int key, Node p, Node pad,boolean sw){
-        if (p!=null && sw){
-            if (p.key<key){
-                buscar(key,p.right,p,sw);
-            }else if(p.key>key){
-                buscar(key,p.left,p,sw);
-            }else{
-                sw = false;
-            }
-        }
-    }
-    
     public void menorDeMayores(Node root) {//Funciona bien
         Node father = null;
         Node node = root.right;
@@ -133,21 +105,6 @@ public class Tree
         else{root.setLeft(node.getLeft());}
         root.setKey(node.getKey()); 
     }
-    
-//    public void delete(Node node){
-//        if (node.left == null && node.right != null) {
-//            menorDeMayores(node);
-//        }
-//        if (node.right == null && node.left != null) {
-//            mayorDeMenores(node);
-//        }
-//        if (node.right != null && node.left != null) {
-//            menorDeMayores(node);
-//        }
-//        if (node.right == null && node.left == null) {
-//            node = null;
-//        }
-//    }
 
     public void remove(Node root, Node father, int key) {
         if (root.getKey() != key) {
@@ -173,64 +130,4 @@ public class Tree
         }
     }
     
-    //El de la profe
-    public void remove(Node root,int key, int opc){
-        Node p = root,pad = null,ant,t;
-        buscar(key,p,pad,true);
-        if (p!=null){
-            if(p.left==null && p.right == null){
-                if(pad == null){
-                    this.root = null;
-                }else{
-                    if(pad.left == p)
-                        pad.left=null;
-                    else
-                        pad.right = null;
-                }
-            }else{
-                if(p.left!=null && p.right!=null){
-                    ant = p;
-                    t = p.left;
-                    while(t.right!=null){
-                        ant = t;
-                        t = t.right;
-                    }
-                    p.key = t.key;
-                    if(ant.key == p.key){
-                        ant.left = t.left;
-                    }else{
-                        ant.right = t.left;
-                    }
-                    p = t;
-                    p.left = null;
-                }else{
-                    if(p.left != null){
-                        if(pad == null){
-                            root = p.left;
-                        }else{
-                            if(pad.right.equals(p)){
-                                pad.right = p.left;
-                            }else{
-                                pad.left = p.left;
-                            }
-                        }
-                        p.left = null;
-                    }else{
-                        if(pad == null){
-                            root = p.right;
-                        }else{
-                            if(p.right.equals(p)){
-                                pad.right = p.right;
-                            }else{
-                                pad.left = p.right;
-                            }
-                        }
-                        p.right = null;
-                    }
-                }
-                p = null;
-            }
-            System.out.println("Nodo eliminado.");
-        }
-    }
 }
